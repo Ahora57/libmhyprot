3ae6865b1094d88aa0e7d40069957745fca43d5f
LazyAhora <84189666+LazyAhora@users.noreply.github.com>
2021-06-07 21:44:07 +0300

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,6 +14,23 @@
 
 using unique_handle = std::unique_ptr<void, decltype(&CloseHandle)>;
 
+
+uintptr_t FindDMAAddy( uint32_t target_process_id , uintptr_t ptr, std::vector<unsigned int> offsets)
+{
+    
+    DWORD    addr = ptr;        //for brevent double value(if type == uintptr_t) (work:0x13371337, but need just 0x1337)
+    for (unsigned int i = 0; i < offsets.size(); ++i)
+    {
+     if(offsets[i] != 0  )      //for prevent break value or just dont add 0
+     addr =    libmhyprot::read_process_memory<DWORD>(target_process_id, addr + offsets[i]);
+    }
+    return addr;
+}
+
+
+
+
+
 uint32_t find_process_id(const std::string_view process_name)
 {
     PROCESSENTRY32 processentry = {};
